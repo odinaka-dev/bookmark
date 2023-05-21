3b23a70f95324fff5f2ac1ebc8b7fdb7a6d5caea
odinaka-dev <110076970+odinaka-dev@users.noreply.github.com>
2023-05-21 08:12:02 -0400

    Add files via upload

---
+++ single.js
@@ -0,0 +1,18 @@
+// var acc = document.getElementsByClassName("accordion");
+// // var enter =document.getElementsByClassName('emd');
+// var i;
+
+// for(i=0; i<acc.length; i++){
+//     acc[i].addEventListener("click", function(){
+//         this.classList.toggle("active");
+
+//         var panel = this.nextElementSibling;
+//         if(panel.style.display === "none"){
+//             panel.style.display="block";
+//             emd.style.color = "black";
+//             // panel.style.color = "black";
+//         }else{
+//             panel.style.display = "none";
+//         }
+//     });
+// }
