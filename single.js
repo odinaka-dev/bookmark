c48287b3837952ca29468cf1548ec22a49ef67bf
odinaka-dev <uchechukwupeter22@gmail.com>
2024-08-14 16:51:10 +0100

    Update single.js

--- single.js
+++ single.js
@@ -1,18 +1,62 @@
-// var acc = document.getElementsByClassName("accordion");
-// // var enter =document.getElementsByClassName('emd');
-// var i;
+// for the navigation styling
+const menuBtn = document.querySelector(".menu");
+const menuClose = document.querySelector(".close-items");
+const menuAll = document.querySelector(".nav-item");
 
-// for(i=0; i<acc.length; i++){
-//     acc[i].addEventListener("click", function(){
-//         this.classList.toggle("active");
+if (!menuAll.classList.contains("add")) {
+  menuBtn.addEventListener("click", () => {
+    menuAll.classList.add("add");
+  });
+}
 
-//         var panel = this.nextElementSibling;
-//         if(panel.style.display === "none"){
-//             panel.style.display="block";
-//             emd.style.color = "black";
-//             // panel.style.color = "black";
-//         }else{
-//             panel.style.display = "none";
-//         }
-//     });
-// }
+menuClose.addEventListener("click", () => {
+  menuAll.classList.remove("add");
+});
+// using html templates to add the second and third section to the section
+// const slidesTemp = [];
+
+const slideBtn = document.querySelectorAll(".slidess-button");
+const firstBtn = document.querySelectorAll(".mySlides");
+
+slideBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("slide-active");
+  });
+});
+
+slideBtn[0].addEventListener("click", () => {
+  firstBtn[0].style.display = "flex";
+  firstBtn[1].style.display = "none";
+  firstBtn[2].style.display = "none";
+});
+
+slideBtn[1].addEventListener("click", () => {
+  firstBtn[0].style.display = "none";
+  firstBtn[1].style.display = "flex";
+  firstBtn[2].style.display = "none";
+});
+
+slideBtn[2].addEventListener("click", () => {
+  firstBtn[0].style.display = "none";
+  firstBtn[1].style.display = "none";
+  firstBtn[2].style.display = "flex";
+});
+
+// the mobile responsive click events
+slideBtn[3].addEventListener("click", () => {
+  firstBtn[0].style.display = "flex";
+  firstBtn[1].style.display = "none";
+  firstBtn[2].style.display = "none";
+});
+
+slideBtn[4].addEventListener("click", () => {
+  firstBtn[0].style.display = "none";
+  firstBtn[1].style.display = "flex";
+  firstBtn[2].style.display = "none";
+});
+
+slideBtn[5].addEventListener("click", () => {
+  firstBtn[0].style.display = "none";
+  firstBtn[1].style.display = "none";
+  firstBtn[2].style.display = "flex";
+});
